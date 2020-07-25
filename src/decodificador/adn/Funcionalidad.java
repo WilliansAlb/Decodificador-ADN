48483219287a2b0afe9f1013d258e291b8501b81
Willians <williansalbertolpez@hotmail.com>
2020-07-25 01:35:17 -0600

    Arreglo de errores de la primera versión de la funcionalidad

--- src/decodificador/adn/Funcionalidad.java
+++ src/decodificador/adn/Funcionalidad.java
@@ -28,7 +28,7 @@ public class Funcionalidad {
         setSecuencia_adn_1(ingreso.nextLine());
         System.out.println("Ingresa la segunda secuencia: ");
         setSecuencia_adn_2(ingreso.nextLine());
-        System.out.println(Decodificar(getSecuencia_adn_1(), getSecuencia_adn_2()));
+        System.out.println(Decodificar_Version2(getSecuencia_adn_1(), getSecuencia_adn_2()));
     }
 
     public String Decodificar(String secuencia1, String secuencia2) {
@@ -38,15 +38,12 @@ public class Funcionalidad {
             if (secuencia2.contains(secuencia_temporal + secuencia1.charAt(i))) {
                 if (i != (secuencia1.length() - 1)) {
                     if (secuencia2.contains(secuencia_temporal + secuencia1.charAt(i) + secuencia1.charAt(i + 1))) {
-
                         secuencia_temporal += secuencia1.charAt(i);
                     } else {
-
                         if (secuencia_temporal.length() > 1) {
                             secuencias_encontradas.add(secuencia_temporal + secuencia1.charAt(i));
                         }
                         secuencia_temporal = secuencia1.charAt(i) + "";
-
                     }
                 } else {
                     secuencias_encontradas.add(secuencia_temporal + secuencia1.charAt(i));
@@ -61,19 +58,24 @@ public class Funcionalidad {
         }
         int posicion_array = 0;
         int tamaño_mayor = 0;
-        for (int i = 0; i < secuencias_encontradas.size(); i++) {
-            if (i == 0) {
-                tamaño_mayor = secuencias_encontradas.get(i).length();
-                posicion_array = i;
-            } else {
-                if (tamaño_mayor <= secuencias_encontradas.get(i).length()) {
+        System.out.println(secuencias_encontradas.size());
+        if (secuencias_encontradas.size() > 0) {
+            for (int i = 0; i < secuencias_encontradas.size(); i++) {
+                if (i == 0) {
                     tamaño_mayor = secuencias_encontradas.get(i).length();
                     posicion_array = i;
+                } else {
+                    if (tamaño_mayor <= secuencias_encontradas.get(i).length()) {
+                        tamaño_mayor = secuencias_encontradas.get(i).length();
+                        posicion_array = i;
+                    }
                 }
+                System.out.println(secuencias_encontradas.get(i));
             }
-
+            return secuencias_encontradas.get(posicion_array);
+        } else {
+            return "NO EXISTEN COINCIDENCIAS";
         }
-        return secuencias_encontradas.get(posicion_array);
     }
 
     public String getSecuencia_adn_1() {
@@ -91,5 +93,65 @@ public class Funcionalidad {
     public void setSecuencia_adn_2(String secuencia_adn_2) {
         this.secuencia_adn_2 = secuencia_adn_2;
     }
-
+    
+    public String Decodificar_Version2(String secuencia_a, String secuencia_b){
+        ArrayList<String> secuencias_encontradas = new ArrayList();
+        String secuencia1 = "";
+        String secuencia2 = "";
+        String secuencia_temporal = "";
+        
+        if (secuencia1.length()>=secuencia2.length()){
+            secuencia1 = secuencia_a;
+            secuencia2 = secuencia_b;
+        } else {
+            secuencia1 = secuencia_b;
+            secuencia2 = secuencia_a;
+        }
+        boolean completado = false;
+        int posicion = 0;
+        
+        while (!completado){
+            secuencia_temporal = secuencia1.charAt(posicion)+"";
+            for (int i = (posicion+1); i < secuencia1.length(); i++){
+                if (secuencia2.contains(secuencia_temporal + secuencia1.charAt(i))){
+                    if (i != (secuencia1.length()-1)){
+                        if (secuencia2.contains(secuencia_temporal + secuencia1.charAt(i) + secuencia1.charAt(i+1))){
+                            secuencia_temporal += secuencia1.charAt(i);
+                        } else {
+                            secuencias_encontradas.add(secuencia_temporal+secuencia1.charAt(i));
+                            posicion++;
+                            break;
+                        }
+                    } else {
+                        secuencias_encontradas.add(secuencia_temporal+secuencia1.charAt(i));
+                        posicion++;
+                        break;
+                    }
+                } else {
+                    posicion++;
+                    break;
+                }
+            }
+            completado = posicion == (secuencia1.length()-1);
+        }
+        int posicion_array = 0;
+        int tamaño_mayor = 0;
+        
+        if (secuencias_encontradas.size() > 0) {
+            for (int i = 0; i < secuencias_encontradas.size(); i++) {
+                if (i == 0) {
+                    tamaño_mayor = secuencias_encontradas.get(i).length();
+                    posicion_array = i;
+                } else {
+                    if (tamaño_mayor <= secuencias_encontradas.get(i).length()) {
+                        tamaño_mayor = secuencias_encontradas.get(i).length();
+                        posicion_array = i;
+                    }
+                }
+            }
+            return secuencias_encontradas.get(posicion_array);
+        } else {
+            return "NO EXISTEN COINCIDENCIAS";
+        }
+    }
 }
